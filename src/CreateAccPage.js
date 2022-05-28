aaca4f0f77c375f64f85454f76cafd4682b25331
Joanna <jiaoni.wang.01@gmail.com>
2022-05-28 17:24:46 -0400

    scss

--- src/CreateAccPage.js
+++ src/CreateAccPage.js
@@ -1,25 +1,20 @@
-import Navbar from './assets/Navbar.js';
-import logo from './assets/logo.svg'
+import logo from './assets/logo_cropped.svg'
 import classes from './styles/CreateAccPage.module.scss'
 
 function CreateAccPage() {
   return (
-    <div className={classes.container}>
-      <Navbar></Navbar>
-      <div className={classes.gridContainer}>
-          <div> 
-          <img src={logo} className={classes.logo} alt="logo" />
-          </div>
-
-          <div className = {classes.gridContainer__createAcc}> 
-          <h1>create account</h1>
-          <form className={classes.gridContainer__createAcc__form}>
-              <input type="text" id="username" value="email"></input>
-              <input type="text" id="username" value="username"></input>
-              <input type="text" id="password" value="password"></input>
-              <input type="submit" value="register"></input>
-          </form>
-          </div>
+    <div className={classes.gridContainer}>
+      <div> 
+        <img src={logo} className={classes.gridContainer__logo} alt="logo" />
+      </div>
+      <div className = {classes.gridContainer__createAcc}> 
+        <h1 className = {classes.gridContainer__createAcc__header}>create account</h1>
+        <form className={classes.gridContainer__createAcc__form}>
+          <input type="text" className={classes.gridContainer__createAcc__text} value="email"></input>
+          <input type="text" className={classes.gridContainer__createAcc__text} value="username"></input>
+          <input type="text" className={classes.gridContainer__createAcc__text} value="password"></input>
+          <input type="submit" className={classes.gridContainer__createAcc__button} value="register"></input>
+        </form>
       </div>
     </div>
   );
