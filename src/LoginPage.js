1d807e671677868b8819e7bc2c76e9e5e9273648
Joanna <jiaoni.wang.01@gmail.com>
2022-05-28 17:02:52 -0400

    styled login page

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,50 +1,27 @@
-import Navbar from './assets/Navbar.js';
-import logo from './assets/logo.svg'
+import logo from './assets/logo_cropped.svg'
 import classes from './styles/LoginPage.module.scss'
 import icon from './assets/phone_icon.svg'
 
 function LoginPage() {
   return (
-    <div className={classes.container}>
-      <Navbar></Navbar>
-      <div className={classes.gridContainer}>
-          <div className = {classes.gridContainer__login}> 
-          <img src={logo} className={classes.logo} alt="logo" />
-          <h1>sonata</h1>
-          <form className={classes.gridContainer__login__form}>
-              <input type="text" id="username" name="username" value="username or email"></input>
-              <input type="text" id="password" name="password" value="password"></input>
-              <input type="submit" value="login"></input>
-          </form>
-          </div>
-
-          <div className = {classes.gridContainer__signUp}> 
-          <img src={icon} className={classes.phoneIcon} alt="icon" />
-          <h2>Don't Have An Account?</h2>
-          <h3>Get Started By Creating Your New Account</h3>
-          <button type="button" className={classes.button}>register</button>
-          </div>
+    <div className={classes.gridContainer}>
+      <div className = {classes.gridContainer__login}> 
+        <img src={logo} className={classes.gridContainer__login__logo} alt="logo" />
+        <h1 className={classes.gridContainer__login__title}>sonata</h1>
+        <form className={classes.gridContainer__login__form}>
+          <input type="text" className={classes.gridContainer__login__form__input} value="username or email"></input>
+          <input type="text" className={classes.gridContainer__login__form__input} value="password"></input>
+          <input type="submit" className={classes.gridContainer__login__form__login} value="login"></input>
+        </form>
+      </div>
+      <div className = {classes.gridContainer__signUp}> 
+        <img src={icon} className={classes.gridContainer__signUp__phoneIcon} alt="icon" />
+        <h2 className={classes.gridContainer__signUp__header1}>Don't Have An Account?</h2>
+        <h3 className={classes.gridContainer__signUp__header2}>Get Started By Creating Your New Account</h3>
+        <button type="button" className={classes.gridContainer__signUp__button}>register</button>
       </div>
     </div>
   );
 }
 
 export default LoginPage;
-
-/*
-<header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload YOUR MOM.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button onClick={()=>alert('GAE')} variant='contained'> GET UR MOM'D </Button> 
-      </header>
-*/
