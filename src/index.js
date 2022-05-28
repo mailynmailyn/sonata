66a6e9a5e23d90af641052b4559e19148291a58a
mailynmailyn <mlpuittinen@gmail.com>
2022-05-28 15:51:30 -0400

    adding url specification in App.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 
-import AboutPage from './AboutPage';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AboutPage />);
+root.render(
+    <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
