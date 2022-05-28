dc12ba54019fc9d13c3770bbf9fcdced2519dc65
Joanna <jiaoni.wang.01@gmail.com>
2022-05-28 17:35:34 -0400

    styled the form

--- src/CreateAccPage.js
+++ src/CreateAccPage.js
@@ -10,10 +10,10 @@ function CreateAccPage() {
       <div className = {classes.gridContainer__createAcc}> 
         <h1 className = {classes.gridContainer__createAcc__header}>create account</h1>
         <form className={classes.gridContainer__createAcc__form}>
-          <input type="text" className={classes.gridContainer__createAcc__text} value="email"></input>
-          <input type="text" className={classes.gridContainer__createAcc__text} value="username"></input>
-          <input type="text" className={classes.gridContainer__createAcc__text} value="password"></input>
-          <input type="submit" className={classes.gridContainer__createAcc__button} value="register"></input>
+          <input type="text" className={classes.gridContainer__createAcc__form__input} value="email"></input>
+          <input type="text" className={classes.gridContainer__createAcc__form__input} value="username"></input>
+          <input type="text" className={classes.gridContainer__createAcc__form__input} value="password"></input>
+          <input type="submit" className={classes.gridContainer__createAcc__form__button} value="register"></input>
         </form>
       </div>
     </div>
