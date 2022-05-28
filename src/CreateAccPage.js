9a797c99d6bc540452d6099bf1f1335fe62d2bc6
Joanna <jiaoni.wang.01@gmail.com>
2022-05-28 18:28:08 -0400

    bg shape added

--- src/CreateAccPage.js
+++ src/CreateAccPage.js
@@ -1,20 +1,24 @@
 import logo from './assets/logo_cropped.svg'
 import classes from './styles/CreateAccPage.module.scss'
+import shape from './assets/createAcc.svg'
 
 function CreateAccPage() {
   return (
-    <div className={classes.gridContainer}>
-      <div> 
-        <img src={logo} className={classes.gridContainer__logo} alt="logo" />
-      </div>
-      <div className = {classes.gridContainer__createAcc}> 
-        <h1 className = {classes.gridContainer__createAcc__header}>create account</h1>
-        <form className={classes.gridContainer__createAcc__form}>
-          <input type="text" className={classes.gridContainer__createAcc__form__input} value="email"></input>
-          <input type="text" className={classes.gridContainer__createAcc__form__input} value="username"></input>
-          <input type="text" className={classes.gridContainer__createAcc__form__input} value="password"></input>
-          <input type="submit" className={classes.gridContainer__createAcc__form__button} value="register"></input>
-        </form>
+    <div className={classes.container}>
+      <img src={shape} className={classes.container__shape} alt="shape" />
+      <div className={classes.container__gridContainer}>
+        <div> 
+          <img src={logo} className={classes.container__gridContainer__logo} alt="logo" />
+        </div>
+        <div className = {classes.container__gridContainer__createAcc}> 
+          <h1 className = {classes.container__gridContainer__createAcc__header}>create account</h1>
+          <form className={classes.container__gridContainer__createAcc__form}>
+            <input type="text" className={classes.container__gridContainer__createAcc__form__input} value="email"></input>
+            <input type="text" className={classes.container__gridContainer__createAcc__form__input} value="username"></input>
+            <input type="text" className={classes.container__gridContainer__createAcc__form__input} value="password"></input>
+            <input type="submit" className={classes.container__gridContainer__createAcc__form__button} value="register"></input>
+          </form>
+        </div>
       </div>
     </div>
   );
