eca62e40681649ef1a16cb1d23d070081534130a
Joanna <jiaoni.wang.01@gmail.com>
2022-05-28 00:35:06 -0400

    HTML elems for the matches page (pre generating the matches) added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 import LoginPage from './LoginPage';
-import CreateAccPage from './CreateAccPage';
+import MatchesPre from './MatchesPre';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<CreateAccPage />);
+root.render(<MatchesPre />);
