153428bca27c93b5158ab8a429590a13e7ce892a
Joanna <jiaoni.wang.01@gmail.com>
2022-05-28 18:49:15 -0400

    styling

--- src/MatchesPre.js
+++ src/MatchesPre.js
@@ -1,11 +1,13 @@
 import Navbar from './assets/Navbar.js';
 import logo from './assets/logo_cropped.svg'
 import classes from './styles/MatchesPre.module.scss'
+import shape from './assets/matches.svg'
 
 function MatchesPre() {
   return (
     <div className={classes.container}>
         <Navbar></Navbar>
+        <img src={shape} className={classes.container__shape} alt="shape" />
         <div className={classes.container__content}>
           <img src={logo} className={classes.container__content__logo} alt="logo" />
           <h1 className={classes.container__content__title}>Your Matches</h1>
