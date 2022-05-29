7d70802ac26c8ab6d454aa8d185d8b9ba7852dc1
mailynmailyn <mlpuittinen@gmail.com>
2022-05-29 03:51:10 -0400

    hoverable buttons

--- src/GenreSelection.js
+++ src/GenreSelection.js
@@ -12,8 +12,47 @@ function GenreSelection(){
                 <h1 className= {classes.header__title}> hello user, what are your favourite genres? </h1>
                 <img src={logo} className= {classes.header__img} alt="logo"/>
             </header>
-            <body>
+            <body className = {classes.body}>
+                <div>
+                    <input type="text" placeholder="Search.."></input>
+                </div>
                 
+                <div className = {classes.grid}>
+                    <button type="button" className={classes.grid__genreButton}> rock </button>
+                    <button type="button" className={classes.grid__genreButton}> indie </button>
+                    <button type="button" className={classes.grid__genreButton}> r and b </button>
+                    <button type="button" className={classes.grid__genreButton}> classical </button>
+                    <button type="button" className={classes.grid__genreButton}> jazz </button>
+                   
+                    <button type="button" className={classes.grid__genreButton}> shoegaze </button>
+                    <button type="button" className={classes.grid__genreButton}> pop </button>
+                    <button type="button" className={classes.grid__genreButton}> folk </button>
+                    <button type="button" className={classes.grid__genreButton}> alternative </button>
+                    <button type="button" className={classes.grid__genreButton}> electronic </button>
+
+                    <button type="button" className={classes.grid__genreButton}> genre </button>
+                    <button type="button" className={classes.grid__genreButton}> genre </button>
+                    <button type="button" className={classes.grid__genreButton}> genre </button>
+                    <button type="button" className={classes.grid__genreButton}> genre </button>
+                    <button type="button" className={classes.grid__genreButton}> genre </button>
+
+                    <button type="button" className={classes.grid__genreButton}> genre </button>
+                    <button type="button" className={classes.grid__genreButton}> genre </button>
+                    <button type="button" className={classes.grid__genreButton}> genre </button>
+                    <button type="button" className={classes.grid__genreButton}> genre </button>
+                    <button type="button" className={classes.grid__genreButton}> genre </button>
+
+                    <button type="button" className={classes.grid__genreButton}> genre </button>
+                    <button type="button" className={classes.grid__genreButton}> genre </button>
+                    <button type="button" className={classes.grid__genreButton}> genre </button>
+                    <button type="button" className={classes.grid__genreButton}> genre </button>
+                    <button type="button" className={classes.grid__genreButton}> genre </button>
+
+                </div>
+
+                <div>
+                    <button type="button" className={classes.body__button}> continue </button>
+                </div>
             </body>
 
         </div>
