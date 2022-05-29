4e570adc4d682df2749e1959aaa556d10207d9e9
mailynmailyn <mlpuittinen@gmail.com>
2022-05-29 02:08:08 -0400

    user message

--- src/GenreSelection.js
+++ src/GenreSelection.js
@@ -7,6 +7,15 @@ function GenreSelection(){
     return(
         <div className = "GenreSelection">
             <Navbar></Navbar>
+            <header className= {classes.header}>
+                <img src={logo} className= {classes.header__img} alt="logo"/>
+                <h1 className= {classes.header__title}> hello user, what are your favourite genres? </h1>
+                <img src={logo} className= {classes.header__img} alt="logo"/>
+            </header>
+            <body>
+                
+            </body>
+
         </div>
     )
 }
