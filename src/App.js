f29b0b3f8e860bc2b53f6171b8e1134a04a74442
Joanna <jiaoni.wang.01@gmail.com>
2022-07-19 20:33:05 -0400

    routing and fixing profile card population for the matches

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import MatchesPost from "./MatchesPost.js";
 import MatchesPre from "./MatchesPre.js";
 import CreateAcc from "./CreateAccPage.js";
 import Navbar from './assets/Navbar.js';
+import json from './data.json';
 
 import {
   BrowserRouter as Router,
@@ -16,42 +17,57 @@ import {
   Route
 } from "react-router-dom";
 
-const pageId = {
-  login: 'login',
-  about: 'about',
-  edit: 'editProfile',
-  select: 'selectGenres',
-  profile: 'profile',
-  create: 'createAccount',
-  matchesPre: 'getmatched',
-  matchesPost: 'matches'
-};
-
 const App = () => {
-  const [currentPage, setPage] = useState('login');
-  const [currentProfile, setCurrentProfile] = useState(5);
-  const defaultProfile = 5;
+  const [currentPage, setPage] = useState('login'); //describes the page to load in
+  const [currentProfile, setCurrentProfile] = useState(6);//describes what profile to populate the profile page with
+  const [loginFlag, setLoginFlag] = useState(false);
 
-  const onNavClick = () => {
-    
+  //function for navbar links
+  const onNavClick = (pageId) => {
+    setPage(pageId);
+    if (pageId == "profile"){
+      setCurrentProfile(6);
+    }
+  }
+
+  //function for finding which profile to populate the page with 
+  const getCurrentProfile = () => {
+    return json.find(profile => profile.id == currentProfile);
+  }
+
+  const onMatchesBtnClicked = () => {
+    setPage('matches');
+  }
+
+  const onLoginBtnClick = () => {
+    setLoginFlag(true);
+    setPage('profile');
+  }
+
+  const onCreateAccBtnClick = () => {
+    setPage('createAccount');
+  }
+
+  const onMatchCardClick = (profileId) => {
+    console.log(profileId);
+    setCurrentProfile(profileId);
+    setPage('profile');
   }
 
   return (
-      <Router>
-      <Routes>
-        <Route path='' element={<LoginPage />} /> 
-        <Route path='/about' element={<AboutPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/editprofile' element={<EditProfile />} />
-        <Route path='/selectgenres' element={<GenreSelection />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/createaccount' element={<CreateAcc />} />
-        <Route path='/getmatched' element={<MatchesPre/>} />
-        <Route path='/matches' element={<MatchesPost/>} />
-      </Routes>
-    </Router>
-    
-  );
+    <div>
+      { loginFlag && <Navbar onNavLinkClick={onNavClick}/> }
+      { currentPage == 'login' && <LoginPage onLoginClick={onLoginBtnClick} onCreateAccClick= {onCreateAccBtnClick} /> }
+      { currentPage == 'createAccount' && <CreateAcc/>}
+      { currentPage == 'about' && <AboutPage/>}
+      { currentPage == 'editProfile' && <EditProfile/>}
+      { currentPage == 'selectGenres' && <GenreSelection/>}
+      { currentPage == 'profile' && <Profile currentProfile={getCurrentProfile()}/>}
+      { currentPage == 'getmatched' && <MatchesPre onMatchesClick={onMatchesBtnClicked}/>}
+      { currentPage == 'matches' && <MatchesPost onCardClick={onMatchCardClick} 
+        allProfiles={json.filter(profile => profile.id!= 6).sort(function (a, b) {return b.percent - a.percent;})}/>}
+    </div>
+  )
 }
 
 export default App;
--- src/GenreSelection.js
+++ src/GenreSelection.js
@@ -6,7 +6,6 @@ import logo from './assets/logo_cropped.svg';
 function GenreSelection(){
     return(
         <div className = "GenreSelection">
-            <Navbar></Navbar>
             <header className= {classes.header}>
                 <img src={logo} className= {classes.header__img} alt="logo"/>
                 <h1 className= {classes.header__title}> hello user, what are your favourite genres of music? </h1>
